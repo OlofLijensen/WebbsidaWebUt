dc2d33326e682c1e521e8f0d18174b314d879537
OlofLijensen <124694647+OlofLijensen@users.noreply.github.com>
2023-10-07 08:51:25 +0000

    Praxis färger

--- process-email.php
+++ process-email.php
@@ -1,21 +1,23 @@
 <?php
+// variabel om hur den anluter samt med vad
 $servername = "localhost";
 $username = "root";
 $password = "NatgranD123!";
 $dbname = "emails";
-$email = $_POST["email"];
+$email = $_POST["email"];  // använder post metod för att få data från html
 
-// Create connection
+// anslutning
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
+// kolla anslutning
+if($conn->connect_error) {
     die("Connection failed: " .$conn->connect_error);
   }
 else{
     echo "connection yes";
 }
 
+// skriver vad för sql kod som ska in och vad för typ av data samt vilken data
 $stmt = $conn->prepare("INSERT INTO emails (emails) VALUES (?)");
 $stmt->bind_param("s", $email);
 $stmt -> execute();
